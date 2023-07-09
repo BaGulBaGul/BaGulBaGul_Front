0cb21e060ee2a864640531b121cc3e8bf54447ba
JYWoooo <wnduddk1119@naver.com>
2023-07-10 01:11:32 +0900

    a태그 수정

--- src/components/layout/postHeader.tsx
+++ src/components/layout/postHeader.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function postHeader() {
     return (
         <div className="fixed top-0 left-0 right-0 flex-row flex w-full justify-between px-[24px] py-[10px] bg-[#FFFFFF]">
-            <img src='/arrow_prev.svg' />
+            <a href="/"><img src='/arrow_prev.svg' /></a>
             <div>
                 파티글 작성하기
             </div>
