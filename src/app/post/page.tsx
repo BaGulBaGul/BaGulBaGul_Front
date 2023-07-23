d530e83768d81abd015088b069356a080bb47e1b
JYWoooo <wnduddk1119@naver.com>
2023-07-23 16:57:57 +0900

    수정 일부 커밋

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,11 +1,11 @@
 import Post from '@/components/pages/post'
 import PostHeader from '@/components/layout/postHeader'
-import Header from '@/components/layout/header';
+import { HeaderNonMain } from '@/components/layout/header'; '@/components/layout/header';
 
 export default function Page() {
     return (
       <>
-      <Header />
+      <HeaderNonMain />
       <PostHeader />
       <Post />
       </>
