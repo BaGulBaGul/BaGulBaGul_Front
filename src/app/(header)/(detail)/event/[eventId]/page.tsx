8ce6fae73b259cbd398b431a6361816f7ebb3c59
eden <whether99@naver.com>
2024-09-19 20:52:53 +0900

    minor fix

--- src/app/(header)/(detail)/event/[eventId]/page.tsx
+++ src/app/(header)/(detail)/event/[eventId]/page.tsx
@@ -4,5 +4,5 @@ import { DetailPage } from '@/components/pages/detail/DetailPage';
 
 export default function Page() {
   const params = useParams()
-  알람 토스트용 AlarmHeader 구현 / DetailPage 컴포넌트 분리 / 캘린더 아이콘 수정  return (<DetailPage origin='event' postId={params.eventId} />)
+  return (<DetailPage origin='event' postId={params.eventId} />)
 }
