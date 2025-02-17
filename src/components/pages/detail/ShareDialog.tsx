6c319b9ecf8a86144fb9735a37b265570ce94780
eden <whether99@naver.com>
2025-02-17 17:26:22 +0900

    minor fix

--- src/components/pages/detail/ShareDialog.tsx
+++ src/components/pages/detail/ShareDialog.tsx
@@ -11,38 +11,16 @@ export default function ShareDialog(props: ShareDialogProps) {
   //   }
   // }, [])
   const handleURL = async () => {
-    // // - using navigator canShare api : 크롬 작동 X, 사파리 작동 O
-    // console.log('- handleURL' + navigator.canShare)
-    // const sharingData = {
-    //   text: '바글바글',
-    //   url: 'http://localhost:3000/',
-    // }
-
-    // try {
-    //   if (navigator.canShare && navigator.canShare(sharingData)) {
-    //     navigator.share(sharingData)
-    //     .then(() => {
-    //       alert('URL이 복사되었습니다.\n공유할 곳에 붙여넣기 해주세요.')
-    //     }).catch(() => {
-    //       console.log('취소')
-    //     })
-    //   }
-    // } catch (e) {
-    //   alert('URL 복사를 실패했습니다.')
-    // }
-
     // - using navigator clipboard api
     try {
       await navigator.clipboard.writeText(process.env.NEXT_PUBLIC_FRONT_BASE_URL + props.sharingURL);
       alert('클립보드에 URL이 복사되었습니다.\n공유할 곳에 붙여넣기 해주세요.')
     } catch (err) {
-      // alert('링크 복사를 실패했습니다.')
-      console.log('링크 복사 실패' + err);
+      alert('링크 복사를 실패했습니다.')
     }
   }
 
   const handleKakao = () => {
-    alert('카카오 공유')
     // // 배포 후 테스트 가능
     // window.Kakao.Share.sendDefault({
     //   objectType: 'feed',
