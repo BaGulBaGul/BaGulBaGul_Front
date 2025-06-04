5234b3984c93d9bac9a9c3eee56fd6b58030b5f7
eden <whether99@naver.com>
2025-06-04 15:10:54 +0900

    Feat: 관리자페이지 게시글작성창 구현 및 추가 리팩토링
    - 본문입력 컴포넌트 수정
    - 날짜스크롤 미선택 시 데이터 처리 수정

--- src/components/pages/write/_Write.tsx
+++ src/components/pages/write/_Write.tsx
@@ -1,6 +1,8 @@
 'use client';
 import { PropsWithChildren } from 'react';
 import { FooterButton } from '@/components/common/_FooterButton';
+import { UseMutationResult } from '@tanstack/react-query';
+import { getCoords } from '.';
 
 // * ====== 250602 alert dialog 추가 적용 필요
 interface Props extends PropsWithChildren { handleSubmit: () => void; }
@@ -20,4 +22,33 @@ export function Write({ handleSubmit, children }: Props) {
         : <></>} */}
     </>
   )
+}
+
+export const handleWrite = (
+  apiURL: string, mutateWrite: UseMutationResult<any, Error, { apiURL: string; body: Object; }, unknown>,
+  body: any, addr?: { full: string, abs: string } | null, edit?: number, prev?: any
+) => {
+  if (!addr && (!!prev && !!prev.data && !!prev.data.event.fullLocation)) { // 공백으로 수정 시 주소 삭제
+    body['abstractLocation'] = null
+    body['fullLocation'] = null
+    body['latitudeLocation'] = null
+    body['longitudeLocation'] = null
+  }
+  if (!!addr && (!edit || (!!prev && !!prev.data && prev.data.event.fullLocation !== addr.full))) {  // 새로운 주소 입력 시 위경도 찾고 등록
+    body['abstractLocation'] = addr.abs
+    body['fullLocation'] = addr.full
+    window.kakao.maps.load(async function () {
+      var geocoder = new window.kakao.maps.services.Geocoder();
+      const coords: any = !!addr ? await getCoords(addr.full, geocoder) : undefined
+      if (!!coords) {
+        body['latitudeLocation'] = coords.La
+        body['longitudeLocation'] = coords.Ma
+      }
+      console.log(body)
+      // mutateWrite.mutate({ apiURL: apiURL, body: body })
+    })
+  } else {
+    console.log(body);
+    // mutateWrite.mutate({ apiURL: apiURL, body: body })
+  }
 }
